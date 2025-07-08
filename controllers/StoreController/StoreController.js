c6b64d55730d65facb046a682ca3a6ce06a73c73
sairam kandru <lohithsairam10@gmail.com>
2025-07-08 21:22:06 +0530

    bcrypt chnage bcrytpjs

--- controllers/StoreController/StoreController.js
+++ controllers/StoreController/StoreController.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const Store = require('../../models/Store');
 const User = require('../../models/userModel');
 const { paginateAndSearch } = require('../../utils/pagination');
